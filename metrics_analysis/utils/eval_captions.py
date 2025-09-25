9afbec25e0d3cf60be65f2ca244934c584fe4611
gabrielmotablima <gabriel.mota.b.lima@gmail.com>
2025-09-25 16:48:47 -0400

    :bug: remove cider trials

--- metrics_analysis/utils/eval_captions.py
+++ metrics_analysis/utils/eval_captions.py
@@ -81,12 +81,12 @@ def compute_individual_metric(predictions, labels, scorer, name):
         #     pbar.set_description(f"Eval. {scorer.name.upper()}")
         # Create an empty dict with empty lists to add the by-example scores
         result = {
-            k:[] for k in list(compute([''], [''], scorer[name]).keys())
+            k:[] for k in list(compute([''], [['']], scorer[name]).keys())
         }
 
         # Compute the score to each example
         for prediction, label in zip(predictions, labels):
-            individual_result = compute([prediction]*repeat, [label], scorer[name])
+            individual_result = compute([prediction*repeat], [label], scorer[name])
 
             # Append the individual scores to the result dict
             for key in individual_result:
@@ -128,14 +128,15 @@ def compute_individual_metrics(control_group, target_group, metrics, images):
     rouge_result  = compute_individual_metric(target_group, control_group, metrics, "rouge")
     meteor_result = compute_individual_metric(target_group, control_group, metrics, "meteor")
     bleu_result   = compute_individual_metric(target_group, control_group, metrics, "bleu")
-    cider_result  = compute_individual_metric(target_group, control_group, metrics, "cider")
+    # cider_result  = compute_individual_metric(target_group, control_group, metrics, "cider")
 
     return {
         **bertscore_result,
         **clipscore_result,
         **rouge_result,
         **meteor_result,
-        **bleu_result
+        **bleu_result,
+        # **cider_result
     }
 
 
